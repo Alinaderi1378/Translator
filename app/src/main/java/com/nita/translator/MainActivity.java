4c3ea469bd8d65917c2abb814dfa1efa0c2d6baa
Ali Naderi <104677416+Alinaderi1378@users.noreply.github.com>
2023-08-12 00:16:02 +0330

    Initial commit

---
+++ app/src/main/java/com/nita/translator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nita.translator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
